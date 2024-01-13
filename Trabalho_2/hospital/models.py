efc59ac166e16f8d925c3856e238e9689cce15f4
Ricardo Araujo <ricarlojo@gmail.com>
2024-01-13 23:24:44 +0000

    models e populate

--- Trabalho_2/hospital/models.py
+++ Trabalho_2/hospital/models.py
@@ -4,24 +4,24 @@ from django.db import models
 
 class Pessoa(models.Model):
     nome = models.CharField(max_length=40, unique=True) # usado como username
-    primeiro_nome = models.CharField(max_length=40)
-    apelido = models.CharField(max_length=200)
-    data_nascimento = models.DateTimeField()
-    genero = models.CharField(max_length=20)
-    telemovel = models.IntegerField(max_length=9)
-    email = models.CharField(max_length=60)
+    primeiro_nome = models.CharField(max_length=40, default="")
+    apelido = models.CharField(max_length=200, default="")
+    data_nascimento = models.DateTimeField(default=None)
+    genero = models.CharField(max_length=20,default="")
+    telemovel = models.IntegerField(default="")
+    email = models.CharField(max_length=60,default="")
     class Meta:
         abstract = True     # Comporta-se como um extends, contudo a tabela pessoa não é criada
 
 class Utente(Pessoa):
-    contacto_emergencia = models.IntegerField(max_length=15)
-    tem_seguro = models.BooleanField()
+    contacto_emergencia = models.IntegerField(default=0)
+    tem_seguro = models.BooleanField(default=False)
 
     def __str__(self):
         return self.nome
 
 class Medico(Pessoa):
-    especialidade = models.CharField(max_length=40)
+    especialidade = models.CharField(max_length=40,default="")
     def __str__(self):
         return self.nome
 
@@ -34,43 +34,51 @@ class Auxiliar(Pessoa):
 
 class Familiar(Pessoa):
     utente = models.ManyToManyField(Utente)   # uma pessoa pode ser familiar de várias
+
+
+
 class Medicamento(models.Model):
     nome = models.CharField(max_length=200, unique=True)
-    dosagem = models.CharField(max_length=40)
-    fabricante = models.CharField(max_length=100)
-    data_validade = models.DateTimeField()
-    precisa_prescricao = models.BooleanField()
+    dosagem = models.CharField(max_length=40, default="")
+    fabricante = models.CharField(max_length=100, default="")
+    precisa_prescricao = models.BooleanField(default="")
 
     def __str__(self):
         return self.nome
 class Medicoes(models.Model):
     utente = models.ForeignKey(Utente, on_delete=models.CASCADE)
-    medico = models.ForeignKey(Medico, on_delete=models.CASCADE)
-    tipo = models.CharField(max_length=40)
-    valor = models.FloatField(max_length=20)
-    unidades = models.CharField(max_length=10)
+    medico = models.ForeignKey(Medico, on_delete=models.CASCADE, null=True)
+    enfermeiro = models.ForeignKey(Enfermeiro, on_delete=models.CASCADE, null=True)
+    tipo = models.CharField(max_length=40, default="")
+    valor = models.FloatField(max_length=20, default="")
+    unidades = models.CharField(max_length=10, default="")
 
-class Prescricoes(Medicamento):
+class Prescricoes(models.Model):
+    enfermeiro = models.ForeignKey(Enfermeiro, on_delete=models.CASCADE, null=True)
+
+    medicamento = models.ManyToManyField(Medicamento)
     utente = models.ForeignKey(Utente, on_delete=models.CASCADE)
     medico = models.ForeignKey(Medico, on_delete=models.CASCADE)
-    inicio_toma = models.DateTimeField()
-    validade_prescricao = models.IntegerField(max_length=2) # deste modo representa o número de meses que será válida
-    dose_diaria = models.IntegerField(max_length=2) # representa o numero de comprimidos por dia
+    inicio_toma = models.DateTimeField(null=True)
+    validade_prescricao = models.IntegerField(default=0)
+    dose_diaria = models.IntegerField(default=0)
 
 class Exame(models.Model):
     utente = models.ForeignKey(Utente, on_delete=models.CASCADE)
-    data_realizacao = models.DateTimeField()
-    nome_exame = models.CharField(max_length=40)
-    resultado = models.CharField(max_length=200)
+    medico = models.ForeignKey(Medico, on_delete=models.CASCADE)
+    enfermeiro = models.ForeignKey(Enfermeiro, on_delete=models.CASCADE, null=True)
+    data_realizacao = models.DateTimeField(null=True)
+    nome_exame = models.CharField(max_length=40, default="")
+    resultado = models.CharField(max_length=200, default="")
 
 class Consulta(models.Model):
     utente = models.ForeignKey(Utente, on_delete=models.CASCADE)
     medico = models.ForeignKey(Medico, on_delete=models.CASCADE)
+    unidade_saude = models.CharField(max_length=200, default="")
+    data = models.DateTimeField(null=True)
+    medicoes = models.ManyToManyField(Medicoes)
     prescricoes = models.ManyToManyField(Prescricoes)
-    unidade_saude = models.CharField(max_length=200)
-    data = models.DateTimeField()
-    medicoes = models.CharField(Medicoes)
-
+    exames = models.ManyToManyField(Exame)
 
 
 
