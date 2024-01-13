8ec0a8227881c7f4835dd4ff728888e135fcf17f
Ricardo Araujo <ricarlojo@gmail.com>
2024-01-13 15:40:52 +0000

    models update

--- Trabalho_2/hospital/models.py
+++ Trabalho_2/hospital/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Pessoa(models.Model):
     nome = models.CharField(max_length=40, unique=True) # usado como username
     primeiro_nome = models.CharField(max_length=40)
-    ultimo_nome = models.CharField(max_length=200)
+    apelido = models.CharField(max_length=200)
     data_nascimento = models.DateTimeField()
     genero = models.CharField(max_length=20)
     telemovel = models.IntegerField(max_length=9)
@@ -16,6 +16,7 @@ class Pessoa(models.Model):
 class Utente(Pessoa):
     contacto_emergencia = models.IntegerField(max_length=15)
     tem_seguro = models.BooleanField()
+
     def __str__(self):
         return self.nome
 
@@ -42,10 +43,35 @@ class Medicamento(models.Model):
 
     def __str__(self):
         return self.nome
+class Medicoes(models.Model):
+    utente = models.ForeignKey(Utente, on_delete=models.CASCADE)
+    medico = models.ForeignKey(Medico, on_delete=models.CASCADE)
+    tipo = models.CharField(max_length=40)
+    valor = models.FloatField(max_length=20)
+    unidades = models.CharField(max_length=10)
+
+class Prescricoes(Medicamento):
+    utente = models.ForeignKey(Utente, on_delete=models.CASCADE)
+    medico = models.ForeignKey(Medico, on_delete=models.CASCADE)
+    inicio_toma = models.DateTimeField()
+    validade_prescricao = models.IntegerField(max_length=2) # deste modo representa o número de meses que será válida
+    dose_diaria = models.IntegerField(max_length=2) # representa o numero de comprimidos por dia
+
+class Exame(models.Model):
+    utente = models.ForeignKey(Utente, on_delete=models.CASCADE)
+    data_realizacao = models.DateTimeField()
+    nome_exame = models.CharField(max_length=40)
+    resultado = models.CharField(max_length=200)
 
 class Consulta(models.Model):
     utente = models.ForeignKey(Utente, on_delete=models.CASCADE)
     medico = models.ForeignKey(Medico, on_delete=models.CASCADE)
-    prescricoes = models.ManyToManyField(Medicamento)
+    prescricoes = models.ManyToManyField(Prescricoes)
     unidade_saude = models.CharField(max_length=200)
     data = models.DateTimeField()
+    medicoes = models.CharField(Medicoes)
+
+
+
+
+
