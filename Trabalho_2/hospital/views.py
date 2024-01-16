a7c7c70b9f3b2c6920005a71fb5639d4ee1096eb
Mónica Martins <monicacarina.martins@gmail.com>
2024-01-16 23:57:44 +0000

    enfermeiro

--- Trabalho_2/hospital/views.py
+++ Trabalho_2/hospital/views.py
@@ -78,9 +78,11 @@ def logout_view(request):
 class IndexView(generic.ListView):
     template_name = "hospital/index.html"
     context_object_name = "lista_consultas"
+
     def test_func(self):
         grupos_utilizador = self.request.user.groups
         return grupos_utilizador.filter(name="Auxiliar").exists()
+
     def handle_no_permission(self):
         return HttpResponseForbidden("Where the hell you think you're going uh?")
 
@@ -91,25 +93,27 @@ class IndexView(generic.ListView):
 class ListaConsultas(LoginRequiredMixin, UserPassesTestMixin, generic.ListView):
     template_name = "hospital/lista_consultas.html"
     context_object_name = "lista_consultas"
-    login_url = 'hospital:user_login'
+    login_url = "hospital:user_login"
+
     def test_func(self):
         grupos_utilizador = self.request.user.groups
         return grupos_utilizador.filter(name="Auxiliar").exists()
+
     def handle_no_permission(self):
         return HttpResponseForbidden("Where the hell you think you're going uh?")
-
 
     def get_queryset(self):
         return Consulta.objects.all()
 
 
-class ListaUtentes(LoginRequiredMixin,UserPassesTestMixin,generic.ListView):
+class ListaUtentes(LoginRequiredMixin, UserPassesTestMixin, generic.ListView):
     template_name = "hospital/index.html"
     context_object_name = "lista_utentes"
 
     def test_func(self):
         grupos_utilizador = self.request.user.groups
         return grupos_utilizador.filter(name="Auxiliar").exists()
+
     def handle_no_permission(self):
         return HttpResponseForbidden("Where the hell you think you're going uh?")
 
@@ -117,33 +121,34 @@ class ListaUtentes(LoginRequiredMixin,UserPassesTestMixin,generic.ListView):
         return Utentes.objects.all()
 
 
-
-
-
 class UtenteView(LoginRequiredMixin, UserPassesTestMixin, generic.DetailView):
     model = Consulta
     template_name = "hospital/utente.html"
     context_object_name = "consultas_utente"
     pk_url_kwarg = "id"
-    login_url = 'hospital:user_login'
-
+    login_url = "hospital:user_login"
 
     def test_func(self):
         grupos_utilizador = self.request.user.groups
         utente = self.get_object()
-        if grupos_utilizador.filter(name="Profissional").exists(): # Se True acede cc nao
+        if grupos_utilizador.filter(
+            name="Profissional"
+        ).exists():  # Se True acede cc nao
             return True
         elif grupos_utilizador.filter(name="Familiar").exists():
             familiar = get_object_or_404(Familiar, nome=self.request.user.username)
             return familiar.utente.filter(nome=utente.nome).exists()
         elif grupos_utilizador.filter(name="Utente").exists():
-            return utente.nome==self.request.user.username
+            return utente.nome == self.request.user.username
         else:
             return False
+
     def get_object(self, queryset=None):
         return get_object_or_404(Utente, id=self.kwargs["id"])
+
     def handle_no_permission(self):
         return HttpResponseForbidden("Where the hell you think you're going uh?")
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         utente = self.object
@@ -152,22 +157,24 @@ class UtenteView(LoginRequiredMixin, UserPassesTestMixin, generic.DetailView):
         return context
 
 
-class MedicoView(LoginRequiredMixin,UserPassesTestMixin, generic.DetailView):
+class MedicoView(LoginRequiredMixin, UserPassesTestMixin, generic.DetailView):
     model = Consulta
     template_name = "hospital/medico.html"
     context_object_name = "consultas_medico"
     pk_url_kwarg = "id"
-    login_url = 'hospital:user_login'
+    login_url = "hospital:user_login"
 
     def test_func(self):
         grupos_utilizador = self.request.user.groups
         medico = self.get_object()
         if grupos_utilizador.filter(name="Medico").exists():
-            return medico.nome==self.request.user.username
+            return medico.nome == self.request.user.username
         else:
             return grupos_utilizador.filter(name="Auxiliar").exists()
+
     def handle_no_permission(self):
         return HttpResponseForbidden("Where the hell you think you're going uh?")
+
     def get_object(self, queryset=None):
         return get_object_or_404(Medico, id=self.kwargs["id"])
 
@@ -179,21 +186,50 @@ class MedicoView(LoginRequiredMixin,UserPassesTestMixin, generic.DetailView):
         return context
 
 
-class FamiliarView(LoginRequiredMixin,UserPassesTestMixin, generic.ListView):
+class EnfermeiroView(LoginRequiredMixin, UserPassesTestMixin, generic.ListView):
+    model = Medicoes
+    template_name = "hospital/enfermeiro.html"
+    context_object_name = "consultas_enfermeiro"
+    login_url = "hospital:user_login"
+    pk_url_kwarg = "id"
+
+    def test_func(self):
+        grupos_utilizador = self.request.user.groups
+        enfermeiro = self.get_object()
+        return grupos_utilizador.filter(name="Enfermeiro").exists()
+
+    def handle_no_permission(self):
+        return HttpResponseForbidden("Where the hell you think you're going uh?")
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(Enfermeiro, id=self.kwargs["id"])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        enfermeiro = self.get_object()
+        consultas_enfermeiro = Medicoes.objects.filter(enfermeiro=enfermeiro)
+        context["consultas_enfermeiro"] = consultas_enfermeiro
+        return context
+
+
+class FamiliarView(LoginRequiredMixin, UserPassesTestMixin, generic.ListView):
     model = Consulta
     template_name = "hospital/index.html"
     context_object_name = "utentes_familiares"
     pk_url_kwarg = "id"
-    login_url = 'hospital:user_login'
+    login_url = "hospital:user_login"
+
     def test_func(self):
         grupos_utilizador = self.request.user.groups
         familiar = self.get_object()
         if grupos_utilizador.filter(name="Familiar").exists():
-            return familiar.nome==self.request.user.username
+            return familiar.nome == self.request.user.username
         else:
             return grupos_utilizador.filter(name="Auxiliar").exists()
+
     def handle_no_permission(self):
         return HttpResponseForbidden("Where the hell you think you're going uh?")
+
     def get_object(self, queryset=None):
         return get_object_or_404(Familiar, id=self.kwargs["id"])
 
@@ -203,27 +239,30 @@ class FamiliarView(LoginRequiredMixin,UserPassesTestMixin, generic.ListView):
         return lista_utentes
 
 
-
-
-
 class AuxiliarView(LoginRequiredMixin, UserPassesTestMixin, generic.ListView):
     template_name = "hospital/index.html"
     context_object_name = "dados"
-    login_url = 'hospital:user_login'
-    def test_func(self): # Como se trata de uma pag igual para todos os auxiliares, e este é o user com mais permisssões, so os membros deste grupo podem aceder
-            return self.request.user.groups.filter(name="Auxiliar").exists()
+    login_url = "hospital:user_login"
+
+    def test_func(
+        self,
+    ):  # Como se trata de uma pag igual para todos os auxiliares, e este é o user com mais permisssões, so os membros deste grupo podem aceder
+        return self.request.user.groups.filter(name="Auxiliar").exists()
+
     def handle_no_permission(self):
         return HttpResponseForbidden("Turn around, nothing to see here")
 
     def get_queryset(self):
         return Consulta.objects.all()
 
+
 class MedicamentoView(LoginRequiredMixin, generic.DetailView):
     model = Medicamento
     template_name = "hospital/medicamento.html"
     context_object_name = "medicamento"
     pk_url_kwarg = "id"
-    login_url = 'hospital:user_login'
+    login_url = "hospital:user_login"
+
     def get_object(self, queryset=None):
         return get_object_or_404(Medicamento, id=self.kwargs["id"])
 
@@ -232,13 +271,15 @@ class ListaUtentesView(LoginRequiredMixin, UserPassesTestMixin, generic.ListView
     model = Utente
     template_name = "hospital/index.html"
     context_object_name = "utentes"
-    login_url = 'hospital:user_login'
+    login_url = "hospital:user_login"
 
     def test_func(self):
         grupos_utilizador = self.request.user.groups
         return grupos_utilizador.filter(name="Profissional").exists()
+
     def handle_no_permission(self):
         return HttpResponseForbidden("Where the hell you think you're going uh?")
+
     def get_queryset(self):
         return Utente.objects.all()
 
@@ -247,11 +288,12 @@ class ListaConsultasView(LoginRequiredMixin, UserPassesTestMixin, generic.ListVi
     model = Consulta
     template_name = "hospital/index.html"
     context_object_name = "consultas"
-    login_url = 'hospital:user_login'
+    login_url = "hospital:user_login"
 
     def test_func(self):
         grupos_utilizador = self.request.user.groups
         return grupos_utilizador.filter(name="Profissional").exists()
+
     def handle_no_permission(self):
         return HttpResponseForbidden("Where the hell you think you're going uh?")
 
@@ -259,14 +301,16 @@ class ListaConsultasView(LoginRequiredMixin, UserPassesTestMixin, generic.ListVi
         return Consulta.objects.all()
 
 
-class ListaMedicosView(LoginRequiredMixin,UserPassesTestMixin, generic.ListView):
+class ListaMedicosView(LoginRequiredMixin, UserPassesTestMixin, generic.ListView):
     model = Medico
     template_name = "hospital/index.html"
     context_object_name = "medicos"
-    login_url = 'hospital:user_login'
+    login_url = "hospital:user_login"
+
     def test_func(self):
         grupos_utilizador = self.request.user.groups
         return grupos_utilizador.filter(name="Auxiliar").exists()
+
     def handle_no_permission(self):
         return HttpResponseForbidden("Where the hell you think you're going uh?")
 
@@ -274,15 +318,16 @@ class ListaMedicosView(LoginRequiredMixin,UserPassesTestMixin, generic.ListView)
         return Medico.objects.all()
 
 
-class ListaFamiliaresView(LoginRequiredMixin,UserPassesTestMixin, generic.ListView):
+class ListaFamiliaresView(LoginRequiredMixin, UserPassesTestMixin, generic.ListView):
     model = Familiar
     template_name = "hospital/index.html"
     context_object_name = "familiares"
-    login_url = 'hospital:user_login'
+    login_url = "hospital:user_login"
 
     def test_func(self):
         grupos_utilizador = self.request.user.groups
         return grupos_utilizador.filter(name="Auxiliar").exists()
+
     def handle_no_permission(self):
         return HttpResponseForbidden("Where the hell you think you're going uh?")
 
@@ -290,14 +335,16 @@ class ListaFamiliaresView(LoginRequiredMixin,UserPassesTestMixin, generic.ListVi
         return Familiar.objects.all()
 
 
-class ListaEnfermeirosView(LoginRequiredMixin,UserPassesTestMixin, generic.ListView):
+class ListaEnfermeirosView(LoginRequiredMixin, UserPassesTestMixin, generic.ListView):
     model = Enfermeiro
     template_name = "hospital/index.html"
     context_object_name = "enfermeiros"
-    login_url = 'hospital:user_login'
+    login_url = "hospital:user_login"
+
     def test_func(self):
         grupos_utilizador = self.request.user.groups
         return grupos_utilizador.filter(name="Auxiliar").exists()
+
     def handle_no_permission(self):
         return HttpResponseForbidden("Where the hell you think you're going uh?")
 
@@ -309,22 +356,7 @@ class ListaMedicamentosView(LoginRequiredMixin, generic.ListView):
     model = Medicamento
     template_name = "hospital/index.html"
     context_object_name = "medicamentos"
-    login_url = 'hospital:user_login'
+    login_url = "hospital:user_login"
+
     def get_queryset(self):
         return Medicamento.objects.all()
-
-
-class EnfermeiroView(LoginRequiredMixin, UserPassesTestMixin, generic.ListView):
-    model = Consulta
-    template_name = "hospital/index.html"
-    context_object_name = "consultas_enfermeiro"
-    login_url = 'hospital:user_login'
-
-    def test_func(self):
-        grupos_utilizador = self.request.user.groups
-        return grupos_utilizador.filter(name="Enfermeiro").exists()
-    def handle_no_permission(self):
-        return HttpResponseForbidden("Where the hell you think you're going uh?")
-
-    def get_queryset(self):
-        return Consulta.objects.all()
--- Trabalho_2/projeto/populate.py
+++ Trabalho_2/projeto/populate.py
@@ -7,8 +7,8 @@ from datetime import datetime
 
 def data_aleatoria(ano_min=1940, ano_limite=2023):
     ano = random.randint(ano_min, ano_limite)
-    mes = random.randint(1,12)
-    dia = random.randint(1,28) # versao simplificada
+    mes = random.randint(1, 12)
+    dia = random.randint(1, 28)  # versao simplificada
     hora = random.randint(0, 23)
     minuto = random.randint(0, 59)
     segundo = random.randint(0, 59)
@@ -18,15 +18,16 @@ def data_aleatoria(ano_min=1940, ano_limite=2023):
 
     return data_aware
 
+
 def telemovel():
-    operadora = random.choice(["91","92","93","96"])
+    operadora = random.choice(["91", "92", "93", "96"])
     resto = "".join(str(random.randrange(0, 9)) for _ in range(7))
     return int(f"{operadora}{resto}")
-    
+
 
 def run():
-    grupo_medicos, created = Group.objects.get_or_create(name='Medico')
-    grupo_utentes, created = Group.objects.get_or_create(name='Utente')
+    grupo_medicos, created = Group.objects.get_or_create(name="Medico")
+    grupo_utentes, created = Group.objects.get_or_create(name="Utente")
     grupo_enfermeiros, created = Group.objects.get_or_create(name="Enfermeiro")
     grupo_profissionais, created = Group.objects.get_or_create(name="Profissional")
     grupo_auxiliares, created = Group.objects.get_or_create(name="Auxiliar")
@@ -34,111 +35,172 @@ def run():
 
     nomes_masculinos = ["João", "Manuel", "Carlos", "António", "Luís"]
     nomes_femininos = ["Maria", "Ana", "Isabel", "Sofia", "Margarida"]
-    apelidos = ["Silva", "Santos", "Oliveira", "Ferreira", "Pereira", "Costa", "Martins", "Rodrigues",
-                            "Almeida", "Ribeiro"]
-    especialidades = ["Cardiologia", "Clinica Geral",
-                               "Pediatria",  "Cirurgia Geral"]
+    apelidos = [
+        "Silva",
+        "Santos",
+        "Oliveira",
+        "Ferreira",
+        "Pereira",
+        "Costa",
+        "Martins",
+        "Rodrigues",
+        "Almeida",
+        "Ribeiro",
+    ]
+    especialidades = ["Cardiologia", "Clinica Geral", "Pediatria", "Cirurgia Geral"]
 
     dosagens = ["500mg", "200mg", "250mg", "20mg", "1000mg", "10mg", "325mg"]
-    fabricantes = ["Johnson & Johnson", "Pfizer", "GlaxoSmithKline",
-                   "Bayer", "Novartis", "Roche", "Sanofi", "Merck"]
+    fabricantes = [
+        "Johnson & Johnson",
+        "Pfizer",
+        "GlaxoSmithKline",
+        "Bayer",
+        "Novartis",
+        "Roche",
+        "Sanofi",
+        "Merck",
+    ]
 
-    for i in range(1,51):  # criar utente e respetivo utilizador
-        genero = random.choice(["M","F"])
-        if genero =="M":
+    for i in range(1, 51):  # criar utente e respetivo utilizador
+        genero = random.choice(["M", "F"])
+        if genero == "M":
             primeiro = random.choice(nomes_masculinos)
         else:
             primeiro = random.choice(nomes_femininos)
 
-        u = Utente(nome=f"utente_{i}",email=f"utente_{i}@email.com",genero=genero,
-                   primeiro_nome=primeiro,apelido=random.choice(apelidos),
-                   data_nascimento=data_aleatoria(ano_limite=2014), telemovel=telemovel(),
-                   contacto_emergencia =telemovel(), tem_seguro = random.choice([True,False]))
-        user = User.objects.create_user(f"utente_{i}",f"utente_{i}@email.com", "password")
+        u = Utente(
+            nome=f"utente_{i}",
+            email=f"utente_{i}@email.com",
+            genero=genero,
+            primeiro_nome=primeiro,
+            apelido=random.choice(apelidos),
+            data_nascimento=data_aleatoria(ano_limite=2014),
+            telemovel=telemovel(),
+            contacto_emergencia=telemovel(),
+            tem_seguro=random.choice([True, False]),
+        )
+        user = User.objects.create_user(
+            f"utente_{i}", f"utente_{i}@email.com", "password"
+        )
         user.groups.add(grupo_utentes)
         user.save()
         u.save()
-    for i in range(1,51):
+    for i in range(1, 51):
         genero = random.choice(["M", "F"])
         if genero == "M":
             primeiro = random.choice(nomes_masculinos)
         else:
             primeiro = random.choice(nomes_femininos)
 
-        m = Medico(nome=f"medico_{i}", email=f"medico_{i}@email.com",genero=genero,
-                   primeiro_nome=primeiro,apelido=random.choice(apelidos),
-                   data_nascimento=data_aleatoria(ano_limite=2014), telemovel=telemovel(),
-                   especialidade=random.choice(especialidades))
-        user = User.objects.create_user(f"medico_{i}", f"medico_{i}@email.com", "password")
-        user.groups.add(grupo_medicos,grupo_profissionais)
+        m = Medico(
+            nome=f"medico_{i}",
+            email=f"medico_{i}@email.com",
+            genero=genero,
+            primeiro_nome=primeiro,
+            apelido=random.choice(apelidos),
+            data_nascimento=data_aleatoria(ano_limite=2014),
+            telemovel=telemovel(),
+            especialidade=random.choice(especialidades),
+        )
+        user = User.objects.create_user(
+            f"medico_{i}", f"medico_{i}@email.com", "password"
+        )
+        user.groups.add(grupo_medicos, grupo_profissionais)
         user.save()
         m.save()
 
-    for i in range(1,51):
+    for i in range(1, 51):
         genero = random.choice(["M", "F"])
         if genero == "M":
             primeiro = random.choice(nomes_masculinos)
         else:
             primeiro = random.choice(nomes_femininos)
 
-        e = Enfermeiro(nome=f"enfermeiro_{i}", email=f"enfermeiro_{i}@email.com",genero=genero,
-                   primeiro_nome=primeiro,apelido=random.choice(apelidos),
-                   data_nascimento=data_aleatoria(ano_limite=2014), telemovel=telemovel())
-        user = User.objects.create_user(f"enfermeiro_{i}",f"enfermeiro_{i}@email.com", "password")
+        e = Enfermeiro(
+            nome=f"enfermeiro_{i}",
+            email=f"enfermeiro_{i}@email.com",
+            genero=genero,
+            primeiro_nome=primeiro,
+            apelido=random.choice(apelidos),
+            data_nascimento=data_aleatoria(ano_limite=2014),
+            telemovel=telemovel(),
+        )
+        user = User.objects.create_user(
+            f"enfermeiro_{i}", f"enfermeiro_{i}@email.com", "password"
+        )
         user.groups.add(grupo_enfermeiros, grupo_profissionais)
         user.save()
         e.save()
 
-    for i in range(1, 51): # Auxiliar
+    for i in range(1, 51):  # Auxiliar
         genero = random.choice(["M", "F"])
         if genero == "M":
             primeiro = random.choice(nomes_masculinos)
         else:
             primeiro = random.choice(nomes_femininos)
 
-        a = Auxiliar(nome=f"auxiliar_{i}", email=f"auxiliar_{i}@email.com", genero=genero,
-                       primeiro_nome=primeiro, apelido=random.choice(apelidos),
-                       data_nascimento=data_aleatoria(ano_limite=2014), telemovel=telemovel())
-        user = User.objects.create_user(f"auxiliar_{i}", f"auxiliar_{i}@email.com", "password")
+        a = Auxiliar(
+            nome=f"auxiliar_{i}",
+            email=f"auxiliar_{i}@email.com",
+            genero=genero,
+            primeiro_nome=primeiro,
+            apelido=random.choice(apelidos),
+            data_nascimento=data_aleatoria(ano_limite=2014),
+            telemovel=telemovel(),
+        )
+        user = User.objects.create_user(
+            f"auxiliar_{i}", f"auxiliar_{i}@email.com", "password"
+        )
         user.groups.add(grupo_auxiliares, grupo_profissionais)
         user.save()
         a.save()
 
-    for i in range(1, 51):   # Familiar
+    for i in range(1, 51):  # Familiar
         genero = random.choice(["M", "F"])
         if genero == "M":
             primeiro = random.choice(nomes_masculinos)
         else:
             primeiro = random.choice(nomes_femininos)
 
-        f = Familiar(nome=f"familiar_{i}", email=f"familiar_{i}@email.com", genero=genero,
-                       primeiro_nome=primeiro, apelido=random.choice(apelidos),
-                       data_nascimento=data_aleatoria(ano_limite=2014), telemovel=telemovel())
+        f = Familiar(
+            nome=f"familiar_{i}",
+            email=f"familiar_{i}@email.com",
+            genero=genero,
+            primeiro_nome=primeiro,
+            apelido=random.choice(apelidos),
+            data_nascimento=data_aleatoria(ano_limite=2014),
+            telemovel=telemovel(),
+        )
         f.save()
         utente = Utente.objects.all().filter(nome=f"utente_{i}").first()
         f.utente.add(utente)
-        user = User.objects.create_user(f"familiar_{i}", f"familiar_{i}@email.com", "password")
+        user = User.objects.create_user(
+            f"familiar_{i}", f"familiar_{i}@email.com", "password"
+        )
         user.groups.add(grupo_familiares)
         user.save()
 
-
     # Fim da criacao dos diferentes tipos de pessoas
 
-    for i in range(1,51):
-        med = Medicamento(nome=f"medicamento {i}", dosagem=random.choice(dosagens),
-                          fabricante=random.choice(fabricantes),
-                          precisa_prescricao=random.choice([True,False]))
+    for i in range(1, 51):
+        med = Medicamento(
+            nome=f"medicamento {i}",
+            dosagem=random.choice(dosagens),
+            fabricante=random.choice(fabricantes),
+            precisa_prescricao=random.choice([True, False]),
+        )
         med.save()
 
-
-
-
-    upcs = ["Hospital de Braga","Hospital de S.João","Hospital de Santa Maria"]
-    for i in range(1,51):
-        u =Utente.objects.all().filter(nome=f"utente_{i}").first()
+    upcs = ["Hospital de Braga", "Hospital de S.João", "Hospital de Santa Maria"]
+    for i in range(1, 51):
+        u = Utente.objects.all().filter(nome=f"utente_{i}").first()
         m = Medico.objects.all().filter(nome=f"medico_{i}").first()
-        c = Consulta(utente = u, medico = m, data=data_aleatoria(2024,2026),
-                     unidade_saude = random.choice(upcs))
+        c = Consulta(
+            utente=u,
+            medico=m,
+            data=data_aleatoria(2024, 2026),
+            unidade_saude=random.choice(upcs),
+        )
         c.save()
         tipos_disponiveis = ["peso", "altura", "PA"]
 
@@ -155,54 +217,102 @@ def run():
             else:
                 valor = random.randrange(80, 160)
                 unidade = "mmHg"
-            medicacao = Medicoes(utente=u,tipo=tipo, valor=valor, unidades=unidade)
+            medicacao = Medicoes(utente=u, tipo=tipo, valor=valor, unidades=unidade)
             medicacao.save()
             if random.randrange(0, 50) > 25:
                 medicacao.medico = m
             else:
-                enfermeiro = Enfermeiro.objects.all().filter(nome=f"enfermeiro_{i}").first()
+                enfermeiro = (
+                    Enfermeiro.objects.all().filter(nome=f"enfermeiro_{i}").first()
+                )
                 medicacao.enfermeiro = enfermeiro
             c.medicoes.add(medicacao)
-        #--------------------------------------------------
+        # --------------------------------------------------
         # Prescrições
 
         for a in range(2):
-
-            medicamento = Medicamento.objects.all().filter(nome=f"medicamento {random.randint(0,50)}").first()
-            prescricao = Prescricoes(utente=u,medico=m, inicio_toma=data_aleatoria(2023, 2024),
-                                      validade_prescricao=random.randint(1, 24),
-                                      dose_diaria=random.randint(1, 7))
+            medicamento = (
+                Medicamento.objects.all()
+                .filter(nome=f"medicamento {random.randint(0,50)}")
+                .first()
+            )
+            prescricao = Prescricoes(
+                utente=u,
+                medico=m,
+                inicio_toma=data_aleatoria(2023, 2024),
+                validade_prescricao=random.randint(1, 24),
+                dose_diaria=random.randint(1, 7),
+            )
             prescricao.save()
             prescricao.medicamento.add(medicamento)
             c.prescricoes.add(prescricao)
 
-        #--------------------------------------------------
+        # --------------------------------------------------
         # Exames
 
-        exames = ["Hemograma", "Colesterol Total", "Glicemia em Jejum",
-                  "Creatinina", "Hemoglobina Glicada"]
+        exames = [
+            "Hemograma",
+            "Colesterol Total",
+            "Glicemia em Jejum",
+            "Creatinina",
+            "Hemoglobina Glicada",
+        ]
 
         exames_resultados = {
-            "Hemograma": ["Normal", "Anemia leve", "Leucocitose", "Trombocitopenia", "Normal"],
-            "Colesterol Total": ["Desejável", "Limítrofe", "Alto", "Muito Alto", "Desejável"],
-            "Glicemia em Jejum": ["Normal", "Pré-diabetes", "Diabetes", "Normal", "Pré-diabetes"],
-            "Creatinina": ["Normal", "Levemente elevada", "Elevada", "Normal", "Levemente elevada"],
-            "Hemoglobina Glicada": ["Normal", "Pré-diabetes", "Diabetes", "Normal", "Pré-diabetes"]
+            "Hemograma": [
+                "Normal",
+                "Anemia leve",
+                "Leucocitose",
+                "Trombocitopenia",
+                "Normal",
+            ],
+            "Colesterol Total": [
+                "Desejável",
+                "Limítrofe",
+                "Alto",
+                "Muito Alto",
+                "Desejável",
+            ],
+            "Glicemia em Jejum": [
+                "Normal",
+                "Pré-diabetes",
+                "Diabetes",
+                "Normal",
+                "Pré-diabetes",
+            ],
+            "Creatinina": [
+                "Normal",
+                "Levemente elevada",
+                "Elevada",
+                "Normal",
+                "Levemente elevada",
+            ],
+            "Hemoglobina Glicada": [
+                "Normal",
+                "Pré-diabetes",
+                "Diabetes",
+                "Normal",
+                "Pré-diabetes",
+            ],
         }
 
         for b in range(2):
             exame = random.choice(exames)
             resultado = random.choice(exames_resultados[exame])
-            e = Exame(utente=u, data_realizacao=data_aleatoria(2023, 2024),
-                       nome_exame=exame, resultado=resultado)
+            e = Exame(
+                utente=u,
+                data_realizacao=data_aleatoria(2023, 2024),
+                nome_exame=exame,
+                resultado=resultado,
+            )
 
             e.save()
             if b == 1:
                 e.medico = m
             else:
-                enfermeiro = Enfermeiro.objects.all().filter(nome=f"enfermeiro_{i}").first()
+                enfermeiro = (
+                    Enfermeiro.objects.all().filter(nome=f"enfermeiro_{i}").first()
+                )
                 e.enfermeiro = enfermeiro
             c.exames.add(e)
-        #--------------------------------------------------
-
-
+        # --------------------------------------------------
